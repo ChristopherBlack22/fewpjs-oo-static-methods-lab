71db7f346e20dee252eff165bfd537e8e333546a
Christopher <christopher.black.2209@googlemail.com>
2021-12-08 02:05:50 +0000

    commit tree 0cd51642688ef327f382c119982f442df71c7be9

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return (string[0].toUpperCase() + string.slice(1))
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^\w|'|\-| ]/g, "")
+  }
+
+  static titleize(string) {
+    let stringArray = string.split(" ");
+    let capsArray = stringArray.map(function(word) {
+      if (word === "the" || word === "a" || word === "an" || word === "but" || word === "of" || word === "and" || word === "for" || word === "at" || word === "by" || word === "from") {
+        return word
+      } else {
+        return this.capitalize(word)
+      }
+    }.bind(Formatter));
+    let capsString = capsArray.join(" ");
+    return this.capitalize(capsString)
+  }
+
 }
